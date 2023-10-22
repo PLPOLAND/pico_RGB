d93660231b77009bd15c4c3d6d2ad1cf3afe3cf2
Marek Pałdyna <marekpaldyna@wp.pl>
2023-10-23 01:00:45 +0200

    feat: RGBController oldTask feat: RGBController color change fade feat: RGBController Fade3/7 feat: RGBController Jump3/7 feat: RGBController more functions done

--- src/RGBController.cpp
+++ src/RGBController.cpp
@@ -2,7 +2,7 @@
 
 RGBController::RGBController()
 {
-    color.set(0, 0, 0);
+    currentColor.set(0, 0, 0);
     redPin = 0;
     greenPin = 1;
     bluePin = 2;
@@ -21,8 +21,14 @@ RGBController::RGBController(byte redPin, byte greenPin, byte bluePin)
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
-    this->color.set(0, 0, 0);
+    this->currentColor.set(0, 0, 0);
+    this->updatePins();
     this->task = nullptr;
+    this->oldTask = nullptr;
+    this->timeMultiplier = TIME_MULTIPLIER_BASE;
+    timer.time(1);//TODO check if value is correct
+    offTimer.time(TIMER_TIME_OFF_BASE);
+    shouldUpdateCurrentColor = false;
 }
 
 RGBController::RGBController(byte redPin, byte greenPin, byte bluePin, Color color)
@@ -30,26 +36,46 @@ RGBController::RGBController(byte redPin, byte greenPin, byte bluePin, Color col
     this->redPin = redPin;
     this->greenPin = greenPin;
     this->bluePin = bluePin;
-    this->color.set(color);
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    this->currentColor.set(color);
+    this->updatePins();
     this->task = nullptr;
+    this->oldTask = nullptr;
+    this->timeMultiplier = TIME_MULTIPLIER_BASE;
+    timer.time(1);//TODO check if value is correct
+    offTimer.time(TIMER_TIME_OFF_BASE);
+    shouldUpdateCurrentColor = false;
 }
+
 
 void RGBController::setColor(Color color)
 {
-    this->color.set(color);
+    this->currentColor.set(color);
     this->updatePins();
+}
+
+void RGBController::setTargetColor(Color color, bool updateBrightness)
+{
+    if (!updateBrightness)
+    {
+        color.setBrightness(this->currentColor.getBrightness());//keep brightness of current color
+    }
+    this->targetColor.set(color);//set target color
+    this->shouldUpdateCurrentColor = true;
 }
 
 Color RGBController::getColor()
 {
-    return this->color;
+    return this->currentColor;
 }
 
 void RGBController::updatePins()
 {
-    setRedPinValue(this->color.getRed());
-    setGreenPinValue(this->color.getGreen());
-    setBluePinValue(this->color.getBlue());
+    setRedPinValue(this->currentColor.getRed());
+    setGreenPinValue(this->currentColor.getGreen());
+    setBluePinValue(this->currentColor.getBlue());
 }
 
 void RGBController::setTask(Task* task)
@@ -58,9 +84,27 @@ void RGBController::setTask(Task* task)
     this->task = task;
 }
 
+void RGBController::bringBackOldTask()
+{
+    if (oldTask != nullptr)
+    {
+        delete task;
+        task = oldTask;
+        oldTask = nullptr;
+    }
+}
+
 void RGBController::clearTask()
 {
-    delete task;
+    if (oldTask != nullptr)
+    {
+        delete oldTask;
+    }
+
+    if (task != nullptr)
+    {
+        oldTask = task;
+    }
     task = nullptr;
 }
 
@@ -70,6 +114,16 @@ void RGBController::tick()
     {
         Serial.println(F("Task is null"));
     }
+
+    if (offTimer.available())
+    {
+        this->turnOnOff();
+    }
+    if (shouldUpdateCurrentColor && !(this->targetColor == this->currentColor) )
+    {
+        this->updateColor();
+    }
+    
     
     if (task != nullptr)
     {
@@ -77,7 +131,10 @@ void RGBController::tick()
         // Serial.println(task->toString().c_str());
         if (task->isChangeColor())
         {
-            this->setColor(task->getColor());
+            if (!targetColor.RGBEqual(task->getColor())){
+                this->setTargetColor(task->getColor());
+                this->shouldUpdateCurrentColor = true;
+                }
 
         }
         else if (task->isOnOff())
@@ -87,89 +144,329 @@ void RGBController::tick()
         }
         else if (task->isBrighter())
         {
-            this->color.setBrightness(this->color.getBrightness() + 10);
+            this->currentColor.setBrightness(this->currentColor.getBrightness() + 10);
+            this->targetColor.setBrightness(this->targetColor.getBrightness() + 10);
             this->updatePins();
-            this->clearTask();
+            this->bringBackOldTask();
         }
         else if (task->isDarker())
         {
-            this->color.setBrightness(this->color.getBrightness() - 10);
+            this->currentColor.setBrightness(this->currentColor.getBrightness() - 10);
+            this->targetColor.setBrightness(this->targetColor.getBrightness() - 10);
             this->updatePins();
-            this->clearTask();
+            this->bringBackOldTask();
         }
         else if (task->isRedUp())
         {
-            this->color.setRed(this->color.getRed() + 1);
+            this->currentColor.setRed(this->currentColor.getRed() + 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isRedDown())
         {
-            this->color.setRed(this->color.getRed() - 1);
+            this->currentColor.setRed(this->currentColor.getRed() - 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isGreenUp())
         {
-            this->color.setGreen(this->color.getGreen() + 1);
+            this->currentColor.setGreen(this->currentColor.getGreen() + 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isGreenDown())
         {
-            this->color.setGreen(this->color.getGreen() - 1);
+            this->currentColor.setGreen(this->currentColor.getGreen() - 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isBlueUp())
         {
-            this->color.setBlue(this->color.getBlue() + 1);
+            this->currentColor.setBlue(this->currentColor.getBlue() + 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isBlueDown())
         {
-            this->color.setBlue(this->color.getBlue() - 1);
+            this->currentColor.setBlue(this->currentColor.getBlue() - 1);
             this->updatePins();
             this->clearTask();
         }
         else if (task->isQuick())
         {
-            //TODO
+            this->timeMultiplier = TIME_MULTIPLIER_FAST;
+            this->bringBackOldTask();
         }
         else if (task->isSlow())
         {
-            //TODO
+            this->timeMultiplier = TIME_MULTIPLIER_BASE;
+            this->bringBackOldTask();
         }
         else if (task->isPause()){
             //TODO
         }
         else if(task->isRainbow()){
-            //TODO
+            if (currentColor.getRed() != 0 && currentColor.getGreen() != 0 && currentColor.getBlue() != 0)
+            {
+                //set min from R G B to 0;
+                if (currentColor.getRed() <= currentColor.getBlue() && currentColor.getRed() <= currentColor.getGreen())
+                {
+                    currentColor.setRed(0);
+                }
+                else if (currentColor.getGreen() <= currentColor.getBlue() && currentColor.getGreen() <= currentColor.getRed())
+                {
+                    currentColor.setGreen(0);
+                }
+                else if (currentColor.getBlue() <= currentColor.getRed() && currentColor.getBlue() <= currentColor.getGreen())
+                {
+                    currentColor.setBlue(0);
+                }
+                else //if something went wrong then set red to 0
+                {
+                    currentColor.setRed(0);
+                }
+                // set max from R G B to 255;
+                if (currentColor.getRed() >= currentColor.getBlue() && currentColor.getRed() >= currentColor.getGreen())
+                {
+                    currentColor.setRed(255);
+                }
+                else if (currentColor.getGreen() >= currentColor.getBlue() && currentColor.getGreen() >= currentColor.getRed())
+                {
+                    currentColor.setGreen(255);
+                }
+                else if (currentColor.getBlue() >= currentColor.getRed() && currentColor.getBlue() >= currentColor.getGreen())
+                {
+                    currentColor.setBlue(255);
+                }
+                else //if something went wrong then set red to 255
+                {
+                    currentColor.setRed(255);
+                }
+                updatePins();
+            }
+            else
+            {
+                if (currentColor.getRed() == 0 && currentColor.getBlue() < 255)
+                {
+                    currentColor.setBlue(currentColor.getBlue() + 1);
+                    currentColor.setGreen(currentColor.getGreen() - 1);
+                }
+                if(currentColor.getBlue() == 0 && currentColor.getGreen() < 255){
+                    currentColor.setRed(currentColor.getRed() - 1);
+                    currentColor.setGreen(currentColor.getGreen() + 1);
+                }
+                if(currentColor.getGreen() == 0 && currentColor.getRed() < 255){
+                    currentColor.setBlue(currentColor.getBlue() - 1);
+                    currentColor.setRed(currentColor.getRed() + 1);
+                }
+                updatePins();
+            }
+            
         }
         else if(task->isBlink()){
-            //TODO
+            Color tmpColor = this->currentColor;
+            // if (currentColor.distance(Color::Red()) < 10)
+            // {
+                Color newColor = Color::Black();
+                setColor(newColor);
+                delay(200);
+                newColor = Color::Red();
+                setColor(newColor);
+                delay(200);
+                newColor = Color::Black();
+                setColor(newColor);
+                delay(200);
+                setColor(tmpColor);//TODO maybe should be target color
+            // }
+            // else{
+
+            // }
+            
+            
         }
-        else if(task->isFade3()){
-            //TODO
+        else if (task->isFade3()) {
+            //TODO timer
+            static byte num = 0;
+            if (num == 0)
+            {
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("FF0600"));
+                num++;
+            }
+            else if (num == 1)
+            {
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("02FF02"));
+                num++;
+            }
+            else
+            {
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("1010FF"));
+                num = 0;
+            }
         }
         else if(task->isFade7()){
-            //TODO
+            //TODO timer
+            static byte num = 0;
+            if (num == 0)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("FF0600"));
+                num++;
+            }
+            else if (num == 1)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("FF1601"));
+                num++;
+            }
+            else if (num == 2)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("FFA601"));
+                num++;
+            }
+            else if (num == 3)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("02FF02"));
+                num++;
+            }
+            else if (num == 4)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("02FF10"));
+                num++;
+            }
+            else if (num == 5)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("02FFA2"));
+                num++;
+            }
+            else if (num == 6)
+            {
+
+                this->shouldUpdateCurrentColor = true;
+                this->setTargetColor(Color::fromHex("1010FF"));
+                num = 0;
+            }
         }
-        else if(task->isJump3()){
-            //TODO
+        else if (task->isJump3()) {
+            //TODO timer
+            static byte num = 0;
+            if (num == 0)
+            {
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("FF0600"));
+                num++;
+            }
+            else if (num == 1)
+            {
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("02FF02"));
+                num++;
+            }
+            else
+            {
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("1010FF"));
+                num = 0;
+            }
         }
-        else if(task->isJump7()){
-            //TODO
+        else if (task->isJump7()) {
+            //TODO timer
+            static byte num = 0;
+            if (num == 0)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("FF0600"));
+                num++;
+            }
+            else if (num == 1)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("FF1601"));
+                num++;
+            }
+            else if (num == 2)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("FFA601"));
+                num++;
+            }
+            else if (num == 3)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("02FF02"));
+                num++;
+            }
+            else if (num == 4)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("02FF10"));
+                num++;
+            }
+            else if (num == 5)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("02FFA2"));
+                num++;
+            }
+            else if (num == 6)
+            {
+                
+                this->shouldUpdateCurrentColor = false;
+                this->setColor(Color::fromHex("1010FF"));
+                num = 0;
+            }
+            
         }
-        else if(task->isDIY1()){
-            //TODO
+        else if (task->isDIY1()) {
+            //TODO timer?
+            Color newColor = Color::fromHSV(rand() % 360, 255, currentColor.getBrightness());
+            this->setTargetColor(newColor);
         }
         else if(task->isDIY2()){
-            //TODO
+
+            //TODO timer?
+            static bool adding = true;
+            this->shouldUpdateCurrentColor = true;
+            if (adding)
+            {
+                this->targetColor.setBrightness(this->targetColor.getBrightness() + 1);
+            }
+            else
+            {
+                this->targetColor.setBrightness(this->targetColor.getBrightness() - 1);
+            }
+            if (this->targetColor.getBrightness() == 100)
+            {
+                adding = false;
+            }
+            else if (this->targetColor.getBrightness() == 0)
+            {
+                adding = true;
+            }
+            
         }
         else if(task->isDIY3()){
-            //TODO
+            offTimer.restart();
+            this->bringBackOldTask();
+            this->setTask(Task::Blink());
         }
         else if(task->isDIY4()){
             //TODO
@@ -197,7 +494,7 @@ void RGBController::turnOnOff()
         this->isOn = false;
     }
     else{
-        this->setColor(this->color);
+        this->setColor(this->currentColor);
         this->isOn = true;
     }
     
@@ -216,4 +513,43 @@ void RGBController::setGreenPinValue(byte green)
 void RGBController::setBluePinValue(byte blue)
 {
     analogWrite(this->bluePin, blue);
+}
+
+void RGBController::updateColor(){
+    if(timer.available()){
+        timer.restart();
+        if (this->currentColor.getRed() < this->targetColor.getRed())
+        {
+            this->currentColor.setRed(this->currentColor.getRed() + 1);
+        }
+        else if (this->currentColor.getRed() > this->targetColor.getRed())
+        {
+            this->currentColor.setRed(this->currentColor.getRed() - 1);
+        }
+        if (this->currentColor.getGreen() < this->targetColor.getGreen())
+        {
+            this->currentColor.setGreen(this->currentColor.getGreen() + 1);
+        }
+        else if (this->currentColor.getGreen() > this->targetColor.getGreen())
+        {
+            this->currentColor.setGreen(this->currentColor.getGreen() - 1);
+        }
+        if (this->currentColor.getBlue() < this->targetColor.getBlue())
+        {
+            this->currentColor.setBlue(this->currentColor.getBlue() + 1);
+        }
+        else if (this->currentColor.getBlue() > this->targetColor.getBlue())
+        {
+            this->currentColor.setBlue(this->currentColor.getBlue() - 1);
+        }
+        if ( this->currentColor.getRed() == this->targetColor.getRed() &&
+            this->currentColor.getGreen() == this->targetColor.getGreen() &&
+            this->currentColor.getBlue() == this->targetColor.getBlue())
+        {
+            this->shouldUpdateCurrentColor = false;
+        }
+    }
+    else{
+        return;
+    }
 }
--- src/RGBController.h
+++ src/RGBController.h
@@ -5,8 +5,10 @@
 #include <Arduino.h>
 #include "Color.h"
 #include "Task.h"
+#include "Stale.h"
+#include "Timers.h"
 
-/// @brief class for controlling RGB LED strip
+/** @brief class for controlling RGB LED strip */
 class RGBController
 {
 public:
@@ -14,30 +16,68 @@ public:
     ~RGBController();
     RGBController(byte redPin, byte greenPin, byte bluePin);
     RGBController(byte redPin, byte greenPin, byte bluePin, Color color);
+
+    /**
+     * @brief Sets the color of the RGB controller. 
+     * 
+     */
     void setColor(Color color);
+
+    
+
+    /** @brief Sets the target color for the RGB controller.
+     * 
+     * @param color The target color to set.
+     * @param updateBrightness Whether or not to update the brightness of the color.
+     */
+    void setTargetColor(Color color, bool updateBrightness = false);
     Color getColor();
     void updatePins();
     void turnOnOff();
 
-    void setTask( Task* task);
+        /** @brief Set task to be executed */
+    void setTask(Task* task);
+
+    /** @brief Removes current task and set old task as task to be executed */
+    void bringBackOldTask();
+
+
+    /** @brief Set old task as current task and clear current task */
     void clearTask();
 
     void tick();
 
 
 private:
-    Color color;
+
+    /** @brief Color that is currently set */
+    Color currentColor;
+    /** @brief Color that is target for current change */
+    Color targetColor;
+
     byte redPin;
     byte greenPin;
     byte bluePin;
+    
+    bool shouldUpdateCurrentColor;
 
+    /** @brief Task that is currently executed */
     Task* task;
+    /** @brief Task that was executed before current task */
+    Task* oldTask;
     
     bool isOn;
+    /** @brief Time multiplier for task execution time */
+    int timeMultiplier;
+
+    Timer timer;
+    Timer offTimer;
 
     void setRedPinValue(byte red);
     void setGreenPinValue(byte green);
     void setBluePinValue(byte blue);
+
+    void updateColor();
 };
 
 #endif
